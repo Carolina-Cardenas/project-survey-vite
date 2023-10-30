fbf32ef47549b35881c1996813f46f3d442c17c3
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2023-10-30 07:47:43 +0100

    work finished

--- src/components/Multistep/MusicType.jsx
+++ src/components/Multistep/MusicType.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
-
 const musicTypes = ["Rock", "Latin music", "Pop" , "Electronic/Dance" , "Blues/Jazz" , "Urban Music" , "Others"];
 export const MusicType = ({updateFormData , value}) => {
-  const [musicType, setMusicType] = useState("")
+  const [musicType, setMusicType] = useState(value)
   const  musicTypeInput = (e) => {updateFormData("musicType" , e.target.value),
 setMusicType(e.target.value)
 }
-    
  return (
       <form>
        <div className="container">
